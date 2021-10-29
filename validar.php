82811c86a265236678f387f8e359931f292ac691
Angel González <angelglz0899@gmail.com>
2021-10-29 13:48:02 -0700

    Commit

--- validar.php
+++ validar.php
@@ -1,6 +1,8 @@
 <?php
 include("./inc/settings.php");
 
+$pdo = new PDO('mysql:host=localhost;dbname=' . $dbname, $username, $password);
+
 if (isset($_POST['username']) && isset($_POST['pwd'])) {
   $username = $_POST['username'];
   $password = $_POST['pwd'];
@@ -12,38 +14,10 @@ if (isset($_POST['username']) && isset($_POST['pwd'])) {
   $stmt->execute();
 }
 
-
-// Create connection
-// $conn = new mysqli($servername, $username, $password, $dbname);
-// Check connection
-
-/*
-function authenticate() {
-  if( isset( $_POST[ 'Connect' ] ) ) {
-    $login = $_POST[ 'login' ];
-    $pass = $_POST[ 'pass' ];
-
-    $query = "SELECT * FROM users WHERE login = :login AND pass = :pass"; // Safe regarding sql injection
-
-    $stmt = $pdo->prepare($query);
-    $stmt->bindParam(":login", $login);
-    $stmt->bindParam(":pass", $pass);
-    $stmt->execute();
-
-    $authenticated = false;
-    if ( $stmt->rowCount() == 1 ) {
-      $authenticated = true;
-    }
-
-    return $authenticated;
-  }
-}*/
-
-// $result = $conn->query($query);
-if ($stmt->rowCount() == 1) {
+if ($stmt->rowCount() == 1 ) {
 
   // output data of each row
-  $row = $result->fetch_assoc();
+  $row = $stmt->fetch(PDO::FETCH_ASSOC);
   session_start();
   $_SESSION["nombre"] = $row["nombre"];
   $_SESSION["apellido1"] = $row["apellido1"];
