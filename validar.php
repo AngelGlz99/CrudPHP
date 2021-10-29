cd978bd367dfe123577e1a3c2f5d14035edbb3a7
Angel González <angelglz0899@gmail.com>
2021-10-29 13:32:21 -0700

    Updates

--- validar.php
+++ validar.php
@@ -1,19 +1,47 @@
 <?php
 include("./inc/settings.php");
-$query="SELECT * FROM usuarios WHERE numero_de_empleado = '$_POST[username]' AND pass= md5('$_POST[pwd]')";
 
+if (isset($_POST['username']) && isset($_POST['pwd'])) {
+  $username = $_POST['username'];
+  $password = $_POST['pwd'];
+  $query = "SELECT * FROM usuarios WHERE numero_de_empleado = :username AND pass= md5(:password)";
+
+  $stmt = $pdo->prepare($query);
+  $stmt->bindParam(":username", $username);
+  $stmt->bindParam(":password", $password);
+  $stmt->execute();
+}
 
 
 // Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
+// $conn = new mysqli($servername, $username, $password, $dbname);
 // Check connection
-if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
-}
 
-$result = $conn->query($query);
-if ($result->num_rows > 0) {
-  
+/*
+function authenticate() {
+  if( isset( $_POST[ 'Connect' ] ) ) {
+    $login = $_POST[ 'login' ];
+    $pass = $_POST[ 'pass' ];
+
+    $query = "SELECT * FROM users WHERE login = :login AND pass = :pass"; // Safe regarding sql injection
+
+    $stmt = $pdo->prepare($query);
+    $stmt->bindParam(":login", $login);
+    $stmt->bindParam(":pass", $pass);
+    $stmt->execute();
+
+    $authenticated = false;
+    if ( $stmt->rowCount() == 1 ) {
+      $authenticated = true;
+    }
+
+    return $authenticated;
+  }
+}*/
+
+// $result = $conn->query($query);
+if ($stmt->rowCount() == 1) {
+
   // output data of each row
   $row = $result->fetch_assoc();
   session_start();
@@ -22,12 +50,11 @@ if ($result->num_rows > 0) {
   $_SESSION["apellido2"] = $row["apellido2"];
 
   header("location: crud.php");
-
 } else {
   echo "Se detecto un acceso ilegal al sistema, su ip esta siendo monitoreada y sera enviada a la policia";
-  ?>
+?>
   <a href="http://localhost/crud/">Sitio de login</a>
-  <?php
+<?php
 }
 $conn->close();
 
